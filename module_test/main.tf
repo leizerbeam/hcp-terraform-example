3ec583388669038d29cc3769fe92fef78db76627
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-12 23:25:09 -0700

    Update main.tf

--- module_test/main.tf
+++ module_test/main.tf
@@ -1,14 +1,3 @@
-# Pre-requisites: configure sensitive environmental variables
-# TFE_TOKEN <- Team API Token
-provider "tfe" {
-  version  = "~> 0.53.0"
-}
-
-# Pre-requisites: configure sensitive environmental variables
-# HCP_CLIENT_ID <- Service Principal Role Admin
-# HCP_CLIENT_SECRET
-provider "hcp" { }
-
 module "hcp_terraform_project_group_access" { 
   source                = "./project_group_access"
   organization_name     = "TFC-Unification-Test-Org-1"
