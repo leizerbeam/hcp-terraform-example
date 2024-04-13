469fb66b5abcf028914f5a556cec8b021facfd6e
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-12 19:40:53 -0700

    test new hcp role bindings

--- main.tf
+++ main.tf
@@ -17,20 +17,18 @@ resource "hcp_project" "provider_test_project" {
 }
 
 # groups don't have a "Manage all projects" permission (this is only at the User role), so we must 
-# create a policy that maps the Contributor role to the Group.
-data "hcp_iam_policy" "group_contributor" {
-  bindings = [
-    {
-      role = "roles/contributor"
-      principals = ["joey-test"] # Name of Team w/ the originating Terraform API Token
-    },
-  ]
+# create a role binding to give the group Contributor role or higher to either org level (NOT AS SAFE)
+resource hcp_organization_iam_binding "group_org_contributor" {
+  principal_id = hcp_group.provider_test_group.resource_id
+  role = "roles/contributor"
 }
 
-# next we must bind the Contributor role to the NEWLY created project so that we can assign another developer group with Terraform read access
-resource "hcp_project_iam_policy" "project_policy" {
-  project_id  = hcp_project.provider_test_project.resource_id
-  policy_data = data.hcp_iam_policy.group_contributor.policy_data
+# groups don't have a "Manage all projects" permission (this is only at the User role), so we must 
+# create a role binding to give the group Contributor role for the project being created (MORE SAFE, BUT MORE CUMBERSOME)
+resource hcp_project_iam_binding "group_project_contributor" {
+  principal_id = hcp_group.provider_test_group.resource_id
+  project_id = hcp_project.provider_test_project.resource_id
+  role = "roles/contributor"
 }
 
 # this works without any special permissions because all groups are visible to all users and groups
@@ -39,13 +37,13 @@ data "tfe_team" "provider_test_tfe_team" {
   organization = "TFC-Unification-Test-Org-1"
 }
 
-# this does not work until we assign my Team API token Project Contributor or higher permission to Group to data source the new tfe_project.id 
+# With project permissions granted, now the Team API token can access the Terraform Project ID
 data "tfe_project" "provider_test_tfe_project" {
   name = hcp_project.provider_test_project.name
   organization = "TFC-Unification-Test-Org-1"
 }
 
-# finally =( - we can assign the Terraform Project Read role for the group to the developer group
+# finally we can assign the Terraform Project Read role for the group to the developer group
 resource "tfe_team_project_access" "admin" {
   access       = "read"
   team_id      = data.tfe_team.provider_test_tfe_team.id
