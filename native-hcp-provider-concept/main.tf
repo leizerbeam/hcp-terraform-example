ef7e0337683b6c27ea8bfdf227faf4ed704b504c
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-15 16:10:20 -0700

    add region example

--- native-hcp-provider-concept/main.tf
+++ native-hcp-provider-concept/main.tf
@@ -20,10 +20,16 @@ resource hcp_terraform_group_project_access "test_group_project_maintainer" {
   access       = "terraform_maintain"
   group_id     = hcp_group.id
   project_id   = hcp_project.id
-  
 }
 
-resource hcp_terraform_workspace "provider_test_workspace" {
-  name = "provider_test_workspace"
-  project_id     = hcp_project.id
+resource hcp_terraform_workspace "provider_test_europe_workspace" {
+  name         = "provider_test_workspace"
+  project_id   = hcp_project.id
+  geo          = "us"
+}
+
+resource hcp_terraform_workspace "provider_test_us_workspace" {
+  name         = "provider_test_workspace"
+  project_id   = hcp_project.id
+  geo          = "eu"
 }
